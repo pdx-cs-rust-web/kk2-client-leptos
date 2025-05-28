4d2a62b2e74270553bcabadeb35f336e170ce634
Bart Massey <bart.massey@gmail.com>
2025-05-27 18:05:47 -0700

    leptos client for knock-knock-2

---
+++ src/fetch_api.rs
@@ -0,0 +1,37 @@
+// This code originally borrowed from the leptos crate
+// examples, where variants appear throughout.
+
+use serde::{Serialize, de::DeserializeOwned};
+
+pub fn fetch_api<T>(
+    path: &str,
+) -> impl std::future::Future<Output = Option<T>> + Send + '_
+where
+    T: Serialize + DeserializeOwned,
+{
+    use leptos::prelude::on_cleanup;
+    use send_wrapper::SendWrapper;
+
+    SendWrapper::new(async move {
+        let abort_controller =
+            SendWrapper::new(web_sys::AbortController::new().ok());
+        let abort_signal = abort_controller.as_ref().map(|a| a.signal());
+
+        // abort in-flight requests if, e.g., we've navigated away from this page
+        on_cleanup(move || {
+            if let Some(abort_controller) = abort_controller.take() {
+                abort_controller.abort()
+            }
+        });
+
+        gloo_net::http::Request::get(path)
+            .abort_signal(abort_signal.as_ref())
+            .send()
+            .await
+            //.map_err(|e| log::error!("{e}"))
+            .ok()?
+            .json()
+            .await
+            .ok()
+    })
+}
