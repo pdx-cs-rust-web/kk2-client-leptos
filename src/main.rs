ee7a5b31161668b24401e323a4d7eb2a30c46e30
Bart Massey <bart.massey@gmail.com>
2025-06-03 15:58:17 -0700

    got example working

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,32 @@ mod joke;
 
 use leptos::prelude::*;
 
-#[component]
-async fn App() -> impl IntoView {
-    let joke = joke::fetch("random-joke").await;
+fn fetch_joke() -> impl IntoView {
+    let joke = LocalResource::new(move || joke::fetch("random-joke"));
     view! {
-        <p>
-        {joke.id}
-        </p>
+        <Transition fallback=|| view! { <div>"Loading..."</div> }> {
+            move || Suspend::new( async move {
+                joke.map(|joke| view! { <p> {joke.id.to_string()} </p> })
+            })}
+        </Transition>
     }
 }
 
-fn main() {
+pub fn main() {
+    use tracing_subscriber::fmt;
+    use tracing_subscriber_wasm::MakeConsoleWriter;
+
+    fmt()
+        .with_writer(
+            // To avoid trace events in the browser from showing their
+            // JS backtrace, which is very annoying, in my opinion
+            MakeConsoleWriter::default()
+                .map_trace_level_to(tracing::Level::DEBUG),
+        )
+        // For some reason, if we don't do this in the browser, we get
+        // a runtime error.
+        .without_time()
+        .init();
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(App)
+    mount_to_body(fetch_joke)
 }
