c8ec56df418cce3994bd7107a1b6914ec188f9b4
Bart Massey <bart.massey@gmail.com>
2025-06-03 18:05:54 -0700

    got full joke working

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 mod joke;
 
+use std::collections::HashSet;
+
 use leptos::prelude::*;
+
+fn format_tags(tags: &HashSet<String>) -> String {
+    let taglist: Vec<&str> = tags.iter().map(String::as_ref).collect();
+    taglist.join(", ")
+}
 
 fn fetch_joke() -> impl IntoView {
     let joke = LocalResource::new(move || joke::fetch("random-joke"));
     view! {
         <Transition fallback=|| view! { <div>"Loading..."</div> }> {
             move || Suspend::new( async move {
-                joke.map(|joke| view! { <p> {joke.id.to_string()} </p> })
+                joke.map(|joke| view! {
+                    <div class="joke">
+                        <span class="teller">{"Knock-Knock!"}</span><br/>
+                        <span class="tellee">{"Who's there?"}</span><br/>
+                        <span class="teller">{joke.whos_there.clone()}</span><br/>
+                        <span class="tellee">{format!("{} who?", &joke.whos_there)}</span><br/>
+                        <span class="teller">{joke.answer_who.clone()}</span>
+                    </div>
+                    <span class="annotation">
+                        {format!("[id: {}", &joke.id)}
+                        {format!("; tags: {}", &format_tags(&joke.tags))}
+                        {format!("; source: {}", &joke.source)}
+                        {"]"}
+                    </span>
+                })
             })}
         </Transition>
     }
